1ee068123f95cc149ae4a0c48fe0ce24431a03c1
Steven Francus <stevenfrancus@Stevens-MacBook-Pro.local>
2015-10-15 20:13:33 -0700

    Transformed Oodt/DratWrapper to inherit from GenericProcess.

--- proteus/backend/Prot2/src/com/company/Start.java
+++ proteus/backend/Prot2/src/com/company/Start.java
@@ -1,17 +1,24 @@
 package com.company;
 
+import java.io.IOException;
+
 /**
  * Created by stevenfrancus on 10/13/15.
  */
 public class Start {
     static int fileCount = 0;
-    private static AbstractDratWrapper dratWrapper = new DratWrapper();
-    private static AbstractOodtWrapper oodtWrapper = new OodtWrapper();
+    private static AbstractDratWrapper dratWrapper = new ProcessDratWrapper();
+    private static AbstractOodtWrapper oodtWrapper = new ProcessOodtWrapper();
     public static void main(String[] args) {
         String dratPath = (args[0] != null) ? args[0] : FileConstants.DRAT_SRC;
-        restartOodt();
+        try {
+            restartOodt();
+        }
+        catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
     }
-    public static void restartOodt() {
+    public static void restartOodt() throws IOException {
         oodtWrapper.stop();
         oodtWrapper.reset();
         oodtWrapper.run();
