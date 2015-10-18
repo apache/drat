a1fce40d2147af9a4b73b53aec80a8a2b591ccbe
emilyl <zhifeili@usc.edu>
2015-10-17 19:39:30 -0700

    file upload

--- proteus/midtier/src/main/java/drat/proteus/HomePage.java
+++ proteus/midtier/src/main/java/drat/proteus/HomePage.java
@@ -4,15 +4,61 @@ import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
 
+import java.io.File;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.upload.FileUpload;
+import org.apache.wicket.markup.html.form.upload.FileUploadField;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
+
+	private FileUploadField fileUpload;
+	private String UPLOAD_FOLDER = "C:\\";
 
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
 
-		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
+		add(new FeedbackPanel("feedback"));
 
-		// TODO Add your page's components here
+		Form<?> form = new Form<Void>("form") {
+			@Override
+			protected void onSubmit() {
+
+				final FileUpload uploadedFile = fileUpload.getFileUpload();
+				if (uploadedFile != null) {
+					// write to a new file
+					File newFile = new File(UPLOAD_FOLDER
+							+ uploadedFile.getClientFileName());
+
+					System.out.println(newFile.getAbsolutePath());
+
+					if (newFile.exists()) {
+						newFile.delete();
+					}
+
+					try {
+						newFile.createNewFile();
+						uploadedFile.writeTo(newFile);
+
+						info("saved file: " + uploadedFile.getClientFileName());
+					} catch (Exception e) {
+						throw new IllegalStateException("Error");
+					}
+				}
+
+			}
+
+		};
+
+		// Enable multipart mode (need for uploads file)
+		form.setMultiPart(true);
+
+		form.add(fileUpload = new FileUploadField("fileUpload"));
+
+		add(form);
+
+		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
 
     }
 }
