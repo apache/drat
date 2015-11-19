c4a9dad193226d5881c78d26edf81ac54dd2c531
Chris Mattmann <mattmann@apache.org>
2015-11-19 10:34:02 -0800

    - Support Git / SVN remote clones and analysis this closes #78

--- proteus/src/main/java/drat/proteus/DratStartForm.java
+++ proteus/src/main/java/drat/proteus/DratStartForm.java
@@ -18,17 +18,28 @@
 package drat.proteus;
 
 import drat.proteus.rest.Unzipper;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.cxf.helpers.FileUtils;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.form.upload.FileUpload;
 import org.apache.wicket.markup.html.form.upload.FileUploadField;
 
+import backend.GenericProcess;
+
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by stevenfrancus on 11/16/15.
  */
 public class DratStartForm extends Form {
+  private static final Logger LOG = Logger.getLogger(DratStartForm.class
+      .getName());
   private FileUploadField fileUploadField;
   private TextField<String> pathField;
 
@@ -82,7 +93,44 @@ public class DratStartForm extends Form {
     dratStarterRunnable.start();
   }
 
-  private void parseAsVersionControlledRepo(String path) {
-    return;
+  private void parseAsVersionControlledRepo(String path) throws IOException {
+    String projectName = null;
+    boolean git = path.endsWith(".git");
+    File tmpDir = new File(System.getProperty("java.io.tmpdir"));
+    String tmpDirPath = tmpDir.getCanonicalPath();
+    String line = null;
+    if (git) {
+      projectName = path.substring(path.lastIndexOf("/") + 1,
+          path.lastIndexOf("."));
+      line = "git clone --depth 1 --branch master " + path;
+    } else {
+      projectName = path.substring(path.lastIndexOf("/") + 1);
+      line = "svn export " + path;
+    }    
+    String clonePath = tmpDirPath + File.separator + projectName;
+    File cloneDir = new File(clonePath);
+    if (cloneDir.isDirectory() && cloneDir.exists()) {
+      LOG.info("Git / SVN clone: [" + clonePath
+          + "] already exists, removing it.");
+      FileUtils.removeDir(cloneDir);
+    }
+    LOG.info("Cloning Git / SVN project: [" + projectName + "] remote repo: ["
+        + path + "] into " + tmpDirPath);
+
+
+    CommandLine cmdLine = CommandLine.parse(line);
+    DefaultExecutor executor = new DefaultExecutor();
+    executor.setWorkingDirectory(tmpDir);
+    int exitValue = executor.execute(cmdLine);
+
+    if (git) {
+      String gitHiddenDirPath = clonePath + File.separator + ".git";
+      File gitHiddenDir = new File(gitHiddenDirPath);
+      LOG.info("Removing .git directory from " + gitHiddenDirPath);
+      FileUtils.removeDir(gitHiddenDir);
+    }
+
+    startDrat(clonePath, "GO");
+
   }
 }
