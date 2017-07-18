d3bc7d684e44701bca6511077e396eb91238a45a
Chris Mattmann <mattmann@apache.org>
2017-07-18 15:56:32 -0700

    Set DRAT specific envs automatically.

---
+++ distribution/src/main/resources/bin/setenv.sh
@@ -0,0 +1,22 @@
+########  setenv.sh ########
+#
+# Set project specific configuration in setenv.sh
+#
+# Example:
+# 		- Change filemgr URL to http://locatlhost:1234
+#			FILEMGR_URL=http://locatlhost:1234
+#
+#		- Set custom job directory
+#			PROJECT_JOB_DIR=/usr/local/project/data/jobs
+#
+############################
+
+export DRAT_HOME=$HOME/drat/deploy
+export FILEMGR_URL=http://localhost:9000
+export WORKFLOW_URL=http://localhost:9001
+export RESMGR_URL=http://localhost:9002
+export FILEMGR_HOME=$DRAT_HOME/filemgr
+export PGE_HOME=$DRAT_HOME/pge
+export PCS_HOME=$DRAT_HOME/pcs
+export OPSUI_URL=http://localhost:8080/opsui
+export SOLR_URL=http://localhost:8080/solr
