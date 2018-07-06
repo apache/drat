1163f6b9b46c43a2b67049f2949907e29a98446e
ahmed ifhaam <ahmedifhaam1@gmail.com>
2018-07-07 02:13:18 +0530

    Updated ProjectList Component to retrieve data from solr

--- webapps/proteus-new/src/main/webapp/resources/src/components/projectstable.vue
+++ webapps/proteus-new/src/main/webapp/resources/src/components/projectstable.vue
@@ -5,19 +5,21 @@
     <!-- <h1>Projects</h1> -->
     <v-data-table id="ttx"
       :headers="headers"
-      :items="desserts"
+      :items="docs"
       hide-actions
       class="elevation-1"
     >
     <template slot="items" slot-scope="props">
-        <td class="text-xs-left" >{{ props.item.num }}</td>
-        <td class="text-xs-left">{{ props.item.repository }}</td>
+        <td class="text-xs-left" >{{props.index+1+count.start}}</td>
+        <td class="text-xs-left">{{ props.item.repo }}</td>
         <td class="text-xs-left">{{ props.item.name }}</td>
         <td class="text-xs-left">{{ props.item.description }}</td>
         <td >
+          <v-btn @click="moreClicked()">
           <v-icon medium 
-          @click="moreClicked()"
+          
           >description</v-icon>
+          </v-btn>
         </td>
       </template>
       </v-data-table>
@@ -26,14 +28,21 @@
 </template>
 
 <script lang="js">
+import axios from 'axios';
+import store from './../store/store';
   export default  {
     name: 'projectstable',
+    store,
     props: [],
     mounted() {
-
+      this.loadData();
+        setInterval(function () {
+          this.loadData();
+        }.bind(this), 30000);
     },
     data() {
       return {
+        
           headers: [
         {
           text: '#',
@@ -46,54 +55,46 @@
         { text: 'Description',sortable: false, value: 'description' },
         { text: 'Audit',sortable: false, value: 'audit' }
       ],
-      desserts: [
-        {
-          num: 1,
-          repository: 'cdm1',
-          name: 'Repository Name',
-          description : 'Description',
-          audit: 'a'
+      count:{
+          numFound :0,
+          start:0
         },
 
-        {
-          num: 1,
-          repository: 'cdm1',
-          name: 'Repository Name',
-          description : 'Description',
-          audit: 'a'
-        },
-        {
-          num: 1,
-          repository: 'cdm1',
-          name: 'Repository Name',
-          description : 'Description',
-          audit: 'a'
-        },
-        {
-          num: 1,
-          repository: 'cdm1',
-          name: 'Repository Name',
-          description : 'Description',
-          audit: 'a'
-        },
-        {
-          num: 1,
-          repository: 'cdm1',
-          name: 'Repository Name',
-          description : 'Description',
-          audit: 'a'
-        }
-
-      ]
+        docs:[{"description":"Default DRAT repo.",
+        "text":["Default DRAT repo.","DRAT"],
+        "repo":"/home/xelvias/drat/src",
+        "loc_url":"http://github.com/chrismattmann/drat.git",
+        "type":"project","id":"http://github.com/chrismattmann/drat.git","name":"DRAT"},
+        {"description":"Default DRAT repo.",
+        "text":["Default DRAT repo.","DRAT"],
+        "repo":"/home/xelvias/drat/src",
+        "loc_url":"http://github.com/chrismattmann/drat.git",
+        "type":"project","id":"http://github.com/chrismattmann/drat.git","name":"DRAT"}]
       }
     },
     methods: {
       moreClicked :function(){
         this.$log.info("as")      
+      },
+      loadData(){
+        
+        axios.get(this.origin+"/solr/statistics/select?q=type:project&rows=220&wt=json")
+            .then(response=>{
+              this.$log.info(response.data);
+              this.docs=response.data.response.docs;
+              this.count.numFound = response.data.response.numFound;
+              this.count.start = response.data.response.start;
+            })
+            .catch(error=>{
+              
+              throw error;
+            })
       }
     },
     computed: {
-
+      origin(){
+        return store.state.origin;
+      }
     }
 }
 </script>
@@ -108,9 +109,7 @@
   tr{background-color :#ddd}
   th{background-color: #2196F3}
 
-  {
-    color: white;
-  }
+  
 
   #tablecard{
     margin-top: 20px;
